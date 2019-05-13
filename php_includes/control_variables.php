4a5cd75952e76979b2930d7523b79a89bdbaecd9
CasualJon <jon@joncyrus.com>
2019-05-13 09:06:58 -0500

    Tweaking how to update strings for engagement checks

--- php_includes/control_variables.php
+++ php_includes/control_variables.php
@@ -1,6 +1,6 @@
 <?php
   //Database informaiton for mysqli access
-  $db_name = "cor_jnd_2";
+  $db_name = "cor_jnd_3test";
   $db_user = "phpmyadmin";
   $db_pass = "westdayton";
 
@@ -11,6 +11,9 @@
   //Intervention config: whether/when to show. 0 = before start, 12 = before 13
   $intervention_trigger = true;
   $intervention_count = 0;
+
+  //Engagement check string data
+  $engagement_string_check = ["09", "9ah"];
 
   //Max allowable survey time in seconds (3600 = 60 min)
   $max_allowed_time = 3600;
